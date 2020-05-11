81b94cacfd78db7102bdd21156eb3e9bd2004de5
Sanzhar Anarbay <anarbay.sanzhar1999@gmail.com>
2020-05-11 18:05:52 +0600

    user profile fixed project details fixed 2

--- src/app/components/auth/user/category/auth-category.component.ts
+++ src/app/components/auth/user/category/auth-category.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {ProjectService} from '../../../../services/project/project.service';
 import {Project} from '../../../../models/project/project';
+import {ProjectLike} from "../../../../models/project/projectLike";
+import {UserService} from "../../../../services/user/user.service";
+import {LikeService} from "../../../../services/like.service";
 
 @Component({
   selector: 'app-category',
@@ -13,7 +16,9 @@ export class AuthCategoryComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private projectService:ProjectService
+    private projectService:ProjectService,
+    private userService: UserService,
+    private likeService:LikeService
   ) {
     router.events.subscribe((val) =>{
       if(val instanceof NavigationEnd){
@@ -34,4 +39,25 @@ export class AuthCategoryComponent implements OnInit {
     })
   }
 
+  like(project:Project){
+    let like:ProjectLike = new ProjectLike();
+    like.user_id = this.userService.getUser().id;
+    like.project_id = project.id;
+
+    this.likeService.createProjectLike(like).subscribe(perf=>{
+      project.likes.push(perf);
+      project.liked = true;
+    })
+  }
+
+  unlike(project:Project){
+    this.likeService.deleteByIdProjectLike(project.id).subscribe(perf=>{
+      console.log(perf);
+      // @ts-ignore
+      project.likes = project.likes.filter(like => like.id!=perf.id);
+      project.liked = false;
+
+
+    })
+  }
 }
